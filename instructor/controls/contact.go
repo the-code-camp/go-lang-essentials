9d2563b1b16051ff9371610c306fa6c646d75636
Ashish Juyal <ashish.juyal@gmail.com>
2024-10-08 11:49:16 +0530

    Switch solution

--- instructor/controls/contact.go
+++ instructor/controls/contact.go
@@ -7,7 +7,7 @@ import (
 )
 
 func contactManager() {
-	err := addContact("first", "last", "phone")
+	err := addContactWithSwitch("first", "last", "phone")
 	if err != nil {
 		fmt.Println("error while adding contact: " + err.Error())
 	} else {
@@ -38,6 +38,27 @@ func addContact(firstName string, lastName string, phone string) error {
 			if err != nil {
 				return errors.New("phone number should only be numeric: " + err.Error())
 			}
+		}
+	}
+	return nil
+}
+
+func addContactWithSwitch(firstName string, lastName string, phone string) error {
+	switch {
+	case firstName == "":
+		return errors.New("first name cannot be blank")
+	case lastName == "":
+		return errors.New("last name cannot be blank")
+	case phone == "":
+		return errors.New("phone cannot be blank")
+	case len(phone) < 10:
+		return errors.New("phone number should be at least 10 digits")
+	case phone[0:1] != "0":
+		return errors.New("phone number should start with zero")
+	default:
+		_, err := strconv.Atoi(phone)
+		if err != nil {
+			return errors.New("phone number should only be numeric: " + err.Error())
 		}
 	}
 	return nil
