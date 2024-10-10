8ecb427cfd3715e9c80eb7128612920ab799c5fc
Ashish Juyal <ashish.juyal@gmail.com>
2024-10-11 02:20:20 +0530

    binding data and behaviour

--- instructor/day-4/concurrency/04-safely-manipulating-data.go
+++ instructor/day-4/concurrency/04-safely-manipulating-data.go
@@ -5,7 +5,21 @@ import (
 	"sync"
 )
 
-var count int
+type Counter struct {
+	sync.Mutex
+	count int
+}
+
+func (c *Counter) Increment() {
+	c.Lock()
+	{
+		c.count++
+	}
+	c.Unlock()
+}
+
+// var count int
+var counter Counter
 
 func main() {
 	wg := &sync.WaitGroup{}
@@ -14,14 +28,17 @@ func main() {
 		go increment(wg)
 	}
 	wg.Wait()
-	fmt.Println("Count: ", count)
+	fmt.Println("Count: ", counter.count)
 }
 
 var mutex sync.Mutex
 
 func increment(wg *sync.WaitGroup) {
 	defer wg.Done()
-	mutex.Lock()
-	count++
-	mutex.Unlock()
+	counter.Increment()
+	// mutex.Lock()
+	// {
+	// 	count++
+	// }
+	// mutex.Unlock()
 }
